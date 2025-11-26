dbd3d77ddc7e37750ee1f7b1153dfe44613a0342
SandraPautonnier <sandrapautonnier@gmail.com>
2025-11-26 17:03:57 +0100

    Update CORS configuration to remove default localhost URL

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Configuration CORS stricte
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
